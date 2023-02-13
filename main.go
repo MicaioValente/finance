311ed5c1f3070be791d185ba1b6c26c7760f8d65
Micaio Valente <micaiovalente@gmail.com>
2023-02-13 00:04:09 -0300

    feat/add gitiignore

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"database/sql"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/micaiovalente/finance/api"
 	db "github.com/micaiovalente/finance/db/sqlc"
 	"log"
+	"os"
 )
 
 func main() {
-	//err := godotenv.Load()
-	//if err != nil {
-	//	log.Fatal("Error loading .env file")
-	//}
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	dbDriver := "postgres"
-	dbSource := "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
-	serverAddress := "0.0.0.0:8000"
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+	serverAddress := os.Getenv("SERVER_ADDRESS")
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
